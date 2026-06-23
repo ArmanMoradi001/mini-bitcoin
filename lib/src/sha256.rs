6ac562a53c9d7d5e0fd2a9be1b297309bca7586e
ArmanMoradi001 <moradi.arman2022@gmail.com>
2026-06-23 21:56:23 +0330

    implement core crypto types, merkle root, and fix SHA256 hashing

--- lib/src/sha256.rs
+++ lib/src/sha256.rs
@@ -1,13 +1,13 @@
-pub struct Hash;
 use crate::U256;
-use serde::Serialize;
+use serde::{Deserialize, Serialize};
 use sha256::digest;
+use std::fmt::Display;
 
-#[derive(Clone, Copy, Serialize)]
+#[derive(Clone, Copy, Serialize, Deserialize, Debug, PartialEq, Eq)]
 pub struct Hash(U256);
 
 impl Hash {
-    pub fn Hash<T: Serialize>(data: &T) -> Self {
+    pub fn hash<T: Serialize>(data: &T) -> Self {
         let mut serilized: Vec<u8> = vec![];
         if let Err(e) = ciborium::into_writer(data, &mut serilized) {
             panic!(
@@ -18,17 +18,23 @@ This should not happen",
         }
 
         let hash = digest(&serilized);
-        let hash_bites = hex::decode(hash).unwrap();
+        let hash_bytes = hex::decode(hash).unwrap();
         let hash_array: [u8; 32] = hash_bytes.as_slice().try_into().unwrap();
 
         Hash(U256::from(hash_array))
     }
 
-    pub fn matches_target(&self, target: U256)-> bool{
+    pub fn matches_target(&self, target: U256) -> bool {
         self.0 <= target
     }
 
-    pub fn zero()-> Self{
+    pub fn zero() -> Self {
         Hash(U256::zero())
+    }
+}
+
+impl Display for Hash {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{:x}", self.0)
     }
 }
--- lib/src/util.rs
+++ lib/src/util.rs
@@ -1,1 +1,26 @@
-pub struct MerkleRoot;
+use crate::sha256::Hash;
+use crate::types::Transaction;
+use serde::{Deserialize, Serialize};
+
+#[derive(Clone, Copy, Serialize, Deserialize, Debug, PartialEq, Eq)]
+pub struct MerkleRoot(Hash);
+
+impl MerkleRoot {
+    pub fn calculate(transactions: &[Transaction]) -> Self {
+        let mut layer: Vec<Hash> = vec![];
+        for transaction in transactions {
+            layer.push(Hash::hash(transaction));
+        }
+        if layer.len() > 1 {
+            let mut new_layer: Vec<Hash> = vec![];
+            for pair in layer.chunks(2) {
+               let left_leaf = pair[0];
+                let right_leaf = pair.get(1).unwrap_or(&pair[0]);
+                new_layer.push(Hash::hash(&[left_leaf, *right_leaf]));
+            }
+            layer = new_layer;
+        }
+
+        MerkleRoot(layer[0])
+    }
+}
